e68cfa6f60a45da45394aa1567089295de6afeca
Jean Carlos Perez Santana <jean.perez@lever.co>
2022-09-11 15:32:37 -0400

    - adding timestamp generator endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,34 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:dateString?",(req,res) => {
+  const dateString = req.params.dateString;
+  let date;
+
+  if(!dateString){
+    date = new Date()
+  }else{
+    if(!isNaN(dateString)){
+      date = new Date(parseInt(dateString))
+    }else{
+      date = new Date(dateString)
+    }
+  }
+
+  if(date.toString() === 'Invalid Date'){
+    res.json({error: date.toString()})
+  }else{
+    res.json({unix: date.getTime(), utc:date.toUTCString()})
+  }
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
